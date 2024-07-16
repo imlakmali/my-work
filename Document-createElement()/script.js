0ec3d76bf6d55a259c993df9b801c5a2f19f98dd
Lakmali Koralage <koralagelak573@gmail.com>
2024-07-16 10:31:46 +0530

    add remove btn

--- Document-createElement()/script.js
+++ Document-createElement()/script.js
@@ -1,9 +1,9 @@
-
 let selectedColor = "";
+
 
 $(document).ready(function () {
 
- $(".color").on('click', function () {
+  $(".color").on("click", function () {
 
     selectedColor = $(this).css("background-color");
     $("#colorTarget").css("background-color", selectedColor);
@@ -21,14 +21,34 @@ $(document).ready(function () {
 
     newDiv.on("click", function () {
 
-      $(this).css("background-color", selectedColor );
+      $(".newDiv").css("border-width", "1px");
+      $(this).css("border-width", "5px");
+      $(this).css("background-color", selectedColor);
 
     });
+
 
     $("#divContainer").append(newDiv);
 
   }
 
+
   $("#addBtn").on("click", addElement);
-  
+
+  $("#removeBtn").on("click", function () {
+
+    $(".newDiv").each(function () {
+      
+      // console.log("-------",$(this).css("border-width"));
+
+      if (parseInt($(this).css("border-width")) > 1) {
+
+        $(this).remove();
+
+      }
+
+    });
+
+  });
+
 });
