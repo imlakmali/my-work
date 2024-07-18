acacd5d55213932ad3553de49e256782232eb515
Lakmali Koralage <koralagelak573@gmail.com>
2024-07-18 14:14:22 +0530

    convert logical way

--- Document-createElement()/script.js
+++ Document-createElement()/script.js
@@ -1,73 +1,114 @@
-let selectedColor = "";
-
-
 $(document).ready(function () {
- 
+
+      let selectedColor = "";
+      let element = [];
+
   $(".color").on("click", function () {
 
-    selectedColor = $(this).css("background-color");
-    $("#colorTarget").css("background-color", selectedColor);
+      selectedColor = $(this).css("background-color");
+      updateColor();
 
   });
-  
-  
 
-  function addElement() {
+  $("#addBtn").on("click", function () {
 
-    const newDiv = $("<div></div>");
-    newDiv.addClass("newDiv");
+      addElement();
+      uiSection();
 
-    const newContent = $("<span>New Content</span>");
-    newDiv.append(newContent);
-
-    newDiv.on("click", function (evnt) {
-
-      if(evnt.ctrlKey){
-
-        const currntBorderwidth = parseInt($(this).css("border-width"));
-
-        if(currntBorderwidth >1){
-
-          $(this).css("border-width", "1px");
-          $(this).css("background-color", "");
-
-        }else{
-
-          $(this).css("border-width", "5px");
-          $(this).css("background-color", selectedColor);
-        }
-      }else{
-
-        $(".newDiv").css("border-width", "1px").css("background-color", "");
-        $(this).css("border-width", "5px").css("background-color", selectedColor);
-
-      }
-     
-      
-    });
-
-
-    $("#divContainer").append(newDiv);
-
-  }
-
-
-  $("#addBtn").on("click", addElement);
+  });
 
   $("#removeBtn").on("click", function () {
 
-    $(".newDiv").each(function () {
-      
-      // console.log("-------",$(this).css("border-width"));
-
-      if (parseInt($(this).css("border-width")) > 1) {
-
-        $(this).remove();
-
-      }
-
-    });
+      removeSelectedDiv();
+      uiSection();
 
   });
 
+  function updateColor() {
+
+       $("#colorTarget").css("background-color", selectedColor);
+
+  }
+
+  function addElement() {
+
+      const newElement = {
+        content: "new content",
+        borderWidth: 1,
+        backgroundColor: "",
+    };
+        element.push(newElement);
+
+  }
+
+  function toggleElementSection(indx, isctrlPressed) {
+
+      if (isctrlPressed) {
+
+        if (element[indx].borderWidth > 1) {
+
+          element[indx].borderWidth = 1;
+          element[indx].backgroundColor = "";
+
+        } else {
+
+          element[indx].borderWidth = 5;
+          element[indx].backgroundColor = selectedColor;
+
+      }
+    } else {
+
+        element.forEach((el, i) => {
+
+          if (i == indx) {
+            el.borderWidth = 5;
+            el.backgroundColor = selectedColor;
+
+          } else {
+
+            el.borderWidth = 1;
+            el.backgroundColor = "";
+
+        }
+
+      });
+
+    }
+
+  }
+
+  function removeSelectedDiv() {
+
+    element = element.filter(function (el) {
+      return el.borderWidth <= 1;
+
+    });
+  }
+
+  function uiSection() {
+
+    $("#divContainer").empty();
+    element.forEach((el, indx) => {
+
+      const newDiv = $("<div></div>");
+      newDiv.addClass("newDiv");
+
+      newDiv.css("border-width", el.borderWidth + "px");
+      newDiv.css("background-color", el.backgroundColor);
+
+      const newContent = $("<span>New Content</span>");
+      newDiv.append(newContent);
+
+      newDiv.on("click", function (evnt) {
+
+        toggleElementSection(indx, evnt.ctrlKey);
+        uiSection();
+
+      });
+
+      $("#divContainer").append(newDiv);
+      
+    });
+
+  }
 });
