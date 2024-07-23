cb36b548f540ee36755adedd34cf533a8eec7c6f
Lakmali Koralage <koralagelak573@gmail.com>
2024-07-23 14:55:58 +0530

    done

--- Document-createElement()/script.js
+++ Document-createElement()/script.js
@@ -2,114 +2,117 @@ $(document).ready(function () {
 
   let selectedColor = ""; 
   let elements = []; 
+  let nextId = 1; 
+
+  // Function to create div elements on the front page
+  function createDivElements() {
 
 
-  // Event listener for color selection
-  $(".color").on("click", function () {
+       $("#divContainer").empty(); 
 
-    selectedColor = $(this).css("background-color"); 
-    updateColor(); 
+      elements.forEach((el) => {
+          const newDiv = $("<div></div>");
+          
+          newDiv.attr("data-id", el.id);
+          newDiv.css("border-width", el.borderWidth + "px");
+          newDiv.css("background-color", el.backgroundColor);
 
+          const newContent = $("<span>" + el.content + "</span>");
+          newDiv.append(newContent);
+
+          // Event listener for toggling element selection
+          newDiv.on("click", function (event) {
+              let id = $(this).attr("data-id");
+              console.log("--div Id", id);
+              console.log("elements",elements)
+              toggleElementSelection(parseInt(id), event.ctrlKey);
+              updateDivElements(); 
+          });
+
+          $("#divContainer").append(newDiv);
+      });
+  }
+
+  // Function to add a new div element
+  function addElement() {
+      const newElement = {
+          id: nextId++, 
+          status: 'inactive', 
+          content: "New Content", 
+          borderWidth: 1,
+          backgroundColor: ""
+      };
+      elements.push(newElement);
+  }
+
+  // Function to update the div elements based on the current state
+  function updateDivElements() {
+      $("#divContainer div").each(function() {
+          const id = $(this).attr("data-id");
+          const el = elements.find(e => e.id === parseInt(id));
+          if (el) {
+              $(this).css("border-width", el.borderWidth + "px");
+              $(this).css("background-color", el.backgroundColor);
+          }
+      });
+  }
+
+  // Event listener for color selection 
+  $("#colorPalette div").on("click", function () {
+      selectedColor = $(this).css("background-color");
+      updateColor();
   });
 
-
-  // Event listener for adding a new element
+  // Event listener for adding a new div element
   $("#addBtn").on("click", function () {
-    addElement(); 
-    renderElements(); 
+      addElement();
+      createDivElements();
   });
 
-  
-  // Event listener for removing selected elements
+  // Event listener for removing selected div elements
   $("#removeBtn").on("click", function () {
-    removeSelectedElements(); 
-    renderElements(); 
+      removeSelectedElements();
+      createDivElements();
   });
-
 
   // Function to update the color target with the selected color
   function updateColor() {
-    $("#colorTarget").css("background-color", selectedColor); 
+      $("#colorTarget").css("background-color", selectedColor);
   }
 
-
-  // Function to add a new element
-  function addElement() {
-    const newElement = {
-      content: "new content", 
-      borderWidth: 1,
-      backgroundColor: ""
-    };
-    elements.push(newElement); 
-  }
-  
-
-  // Function to toggle the selection state of an element
-  function toggleElementSelection(index, isCtrlPressed) {
-    if (isCtrlPressed) {
-
-      // If Ctrl key is pressed, toggle the selection state
-      if (elements[index].borderWidth > 1) {
-
-        elements[index].borderWidth = 1; 
-        elements[index].backgroundColor = ""; 
-      } else {
-
-        elements[index].borderWidth = 5; 
-        elements[index].backgroundColor = selectedColor; 
-      }
-    } else {
-
-      // If Ctrl key is not pressed, select only the clicked element
-      elements.forEach((el, i) => {
-
-        if (i == index) {
-          el.borderWidth = 5; 
-          el.backgroundColor = selectedColor; 
-        } else {
-
-          el.borderWidth = 1; 
-          el.backgroundColor = ""; 
-        }
-      });
-    }
-  }
-
-
-  // Function to remove selected elements
+  // Function to remove selected div elements
   function removeSelectedElements() {
-
-    elements = elements.filter(function (el) {
-      return el.borderWidth <= 1; 
-
-    });
+      elements = elements.filter((el) => el.status !== 'active');
   }
 
-
-  // Function to render elements on the UI
-  function renderElements() {
-
-    $("#divContainer").empty(); 
-    elements.forEach((el, index) => {
-
-      const newDiv = $("<div></div>");
-      newDiv.addClass("newDiv"); 
-
-      newDiv.css("border-width", el.borderWidth + "px");
-      newDiv.css("background-color", el.backgroundColor); 
-
-      const newContent = $("<span>New Content</span>");
-      newDiv.append(newContent); 
-
-
-      // Event listener for toggling element selection
-      newDiv.on("click", function (event) {
-        toggleElementSelection(index, event.ctrlKey); 
-        renderElements(); 
+  // Function to toggle the selection state of a div element
+  function toggleElementSelection(id, isCtrlPressed) {
+      elements.forEach((el) => {
+          if (isCtrlPressed) {
+              // If Ctrl key is pressed
+              if (el.id === id) {
+                  if (el.borderWidth > 1) {
+                      el.borderWidth = 1;
+                      el.backgroundColor = "";
+                      el.status = 'inactive';
+                  } else {
+                      el.borderWidth = 5;
+                      el.backgroundColor = selectedColor;
+                      el.status = 'active';
+                  }
+              }
+          } else {
+              // If Ctrl key is not pressed, select only the clicked element
+              if (el.id === id) {
+                  el.borderWidth = 5;
+                  el.backgroundColor = selectedColor;
+                  el.status = 'active';
+              } else {
+                  el.borderWidth = 1;
+                  el.backgroundColor = "";
+                  el.status = 'inactive';
+              }
+          }
       });
-
-      $("#divContainer").append(newDiv); 
-    });
   }
-
 });
--- test01/script.js
+++ test01/script.js
@@ -1,146 +1,119 @@
+
+
 $(document).ready(function () {
 
-  let selectedColor = ""; 
-  let elements = []; 
-  let nextId = 1; 
-  // console.log(elements)----
+    let selectedColor = ""; 
+    let elements = []; 
+    let nextId = 1; 
 
-  // Function to add a new div element
-  function addElement() {
-    const newElement = {
-      id: nextId++, 
-      status: 'inactive', 
-      content: "new content", 
-      borderWidth: 1,
-      backgroundColor: ""
-    };
-    elements.push(newElement); 
-  }
+    // Function to create div elements on the front page
+    function createDivElements() {
 
+        $("#divContainer").empty(); 
 
-  // Event listener for color selection 
-  $("#colorPalette div").on("click", function () {
+        elements.forEach((el) => {
+            const newDiv = $("<div></div>");
+            
+            newDiv.attr("data-id", el.id);
+            newDiv.css("border-width", el.borderWidth + "px");
+            newDiv.css("background-color", el.backgroundColor);
 
-    selectedColor = $(this).css("background-color"); 
-    updateColor(); 
+            const newContent = $("<span>" + el.content + "</span>");
+            newDiv.append(newContent);
 
-  });
+            // Event listener for toggling element selection
+            newDiv.on("click", function (event) {
+                let id = $(this).attr("data-id");
+                console.log("--div Id", id);
+                console.log("elements",elements)
+                toggleElementSelection(parseInt(id), event.ctrlKey);
+                updateDivElements(); 
+            });
 
+            $("#divContainer").append(newDiv);
+        });
+    }
 
-  // Event listener for adding a new div element
-  $("#addBtn").on("click", function () {
+    // Function to add a new div element
+    function addElement() {
+        const newElement = {
+            id: nextId++, 
+            status: 'inactive', 
+            content: "New Content", 
+            borderWidth: 1,
+            backgroundColor: ""
+        };
+        elements.push(newElement);
+    }
 
-    addElement(); 
-    renderElements(); 
+    // Function to update the div elements based on the current state
+    function updateDivElements() {
+        $("#divContainer div").each(function() {
+            const id = $(this).attr("data-id");
+            const el = elements.find(e => e.id === parseInt(id));
+            if (el) {
+                $(this).css("border-width", el.borderWidth + "px");
+                $(this).css("background-color", el.backgroundColor);
+            }
+        });
+    }
 
-  });
-
-
-  // Event listener for removing selected div elements
-  $("#removeBtn").on("click", function () {
-
-    removeSelectedElements(); 
-    renderElements(); 
-
-  });
-
-
-  // Function to update the color target with the selected color
-  function updateColor() {
-
-    $("#colorTarget").css("background-color", selectedColor); 
-
-  }
-
-
-  // Function to toggle the selection state of an div element
-  function toggleElementSelection(id, isCtrlPressed) {
-
-    elements.forEach((el) => {
-
-      if (isCtrlPressed) {
-
-        // If Ctrl key is pressed
-        if (el.id === id) {
-
-          if (el.borderWidth > 1) {
-
-            el.borderWidth = 1; 
-            el.backgroundColor = ""; 
-            el.status = 'inactive';
-
-          } else {
-
-            el.borderWidth = 5; 
-            el.backgroundColor = selectedColor; 
-            el.status = 'active'; 
-
-          }
-        }
-      } else {
-
-        // If Ctrl key is not pressed, select only the clicked element
-        if (el.id === id) {
-
-          el.borderWidth = 5; 
-          el.backgroundColor = selectedColor; 
-          el.status = 'active'; 
-
-        } else {
-
-          el.borderWidth = 1; 
-          el.backgroundColor = ""; 
-          el.status = 'inactive'; 
-
-        }
-      }
-    });
-  }
-
-
-  // Function to remove selected div elements
-  function removeSelectedElements() {
-
-    elements = elements.filter((el) => el.status !== 'active'); 
-
-  }
-
-
-  // Function to render elements on the UI
-  function renderElements() {
-
-    $("#divContainer").empty(); 
-
-    elements.forEach((el) => {
-
-      const newDiv = $("<div></div>");
-      newDiv.addClass("newDiv"); 
-
-      newDiv.attr("data-id", el.id); 
-
-      newDiv.css("border-width", el.borderWidth + "px");
-      newDiv.css("background-color", el.backgroundColor); 
-
-      const newContent = $("<span>New Content</span>");
-      newDiv.append(newContent); 
-
-
-      // Event listener for toggling element selection
-      newDiv.on("click", function (event) {
-
-        let id = $(this).attr("data-id"); 
-        console.log("--div id",id  )
-        console.log(`Element ID ${el.id} status: ${el.status}`);
-        toggleElementSelection(parseInt(id), event.ctrlKey); 
-        renderElements(); 
-
-      });
-
-      $("#divContainer").append(newDiv); 
+    // Event listener for color selection 
+    $("#colorPalette div").on("click", function () {
+        selectedColor = $(this).css("background-color");
+        updateColor();
     });
 
-    
-    
-  }
+    // Event listener for adding a new div element
+    $("#addBtn").on("click", function () {
+        addElement();
+        createDivElements();
+    });
 
+    // Event listener for removing selected div elements
+    $("#removeBtn").on("click", function () {
+        removeSelectedElements();
+        createDivElements();
+    });
+
+    // Function to update the color target with the selected color
+    function updateColor() {
+        $("#colorTarget").css("background-color", selectedColor);
+    }
+
+    // Function to remove selected div elements
+    function removeSelectedElements() {
+        elements = elements.filter((el) => el.status !== 'active');
+    }
+
+    // Function to toggle the selection state of a div element
+    function toggleElementSelection(id, isCtrlPressed) {
+        elements.forEach((el) => {
+            if (isCtrlPressed) {
+                // If Ctrl key is pressed
+                if (el.id === id) {
+                    if (el.borderWidth > 1) {
+                        el.borderWidth = 1;
+                        el.backgroundColor = "";
+                        el.status = 'inactive';
+                    } else {
+                        el.borderWidth = 5;
+                        el.backgroundColor = selectedColor;
+                        el.status = 'active';
+                    }
+                }
+            } else {
+                // If Ctrl key is not pressed, select only the clicked element
+                if (el.id === id) {
+                    el.borderWidth = 5;
+                    el.backgroundColor = selectedColor;
+                    el.status = 'active';
+                } else {
+                    el.borderWidth = 1;
+                    el.backgroundColor = "";
+                    el.status = 'inactive';
+                }
+            }
+        });
+    }
 });
