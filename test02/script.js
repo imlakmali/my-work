02c6cae907f3155ca7079984861baf8be44d38eb
Lakmali Koralage <koralagelak573@gmail.com>
2024-07-24 15:24:41 +0530

    add comments

--- test02/script.js
+++ test02/script.js
@@ -1,59 +1,70 @@
+let idCounter = 0;
+const elements = [];
+
+
 $(document).ready(function(){
 
-    let idCounter = 0;
-    const elements = [];
+    // Event listener for add button.
+     $("#addButton").click(function(){
 
-    $("#addButton").click(function(){
+        const newElement = addElement();   //create new element and add it in to the array.
+        createNewDivElement(newElement); 
+        
+    });
 
-        idCounter++;
-        const newDivId= `Div-Element ID : ${idCounter}`
+   
+});
 
-        function addElement(){
+//  add new element object to elements array
+function addElement(){
 
-            const newElement ={
-                id: newDivId,
-                active: false,
-                borderThickness: 1
-               
-            };
-            elements.push(newElement);
-            return newElement
-        }
+    idCounter++;
+    const newDivId = `Div-Element ID : ${idCounter}`;    
+    const newElement ={
+        id: newDivId,
+        active: false,
+        borderThickness: 1
+       
+    };
+    elements.push(newElement);
+    return newElement
 
+}
 
+//  create new div element in the DOM
 function createNewDivElement(element){
 
-     $("<div></div>")
-    .addClass("new-div")
-    .attr("data-id", element.id)
-    .css("border",`${element.borderThickness}px solid #140f44` )
-    .text(`Div ID: ${element.id}`)
-    .appendTo("#divContainer")
-    .click(function(){
-        updateDivStatus(element.id)
-    })
+    $("<div></div>")
+   .addClass("new-div")
+   .attr("data-id", element.id)
+   .css("border",`${element.borderThickness}px solid #140f44` )
+   .text(`Div ID: ${element.id}`)
+   .appendTo("#divContainer")
+   .click(function(){
+
+       updateDivStatus(element.id);
+
+   })
 }
 
+
+// update the status of s div element
 function updateDivStatus(id){
-    const element = elements.find(e=> e.id === id);
-    if(element){
-        element.active = !element.active;
 
-        let borderThickness;
-        if(element.active){
-            borderThickness = 10;
-        }else{
-            borderThickness=1;
-        }
-        $(`div[data-id="${id}"]`).css("border-width", `${borderThickness}px`);
-        console.log(elements)
+const element = elements.find(e=> e.id === id);
+if(element){
+
+    element.active = !element.active;
+
+    let borderThickness;
+    if(element.active){
+        borderThickness = 10;
+    }else{
+        borderThickness=1;
     }
+    $(`div[data-id="${id}"]`).css("border-width", `${borderThickness}px`);
+    console.log(elements)
+    
 }
-        
-const newElement = addElement(); 
-createNewDivElement(newElement); 
-
-     
-
-    });
-});
+}
+    
