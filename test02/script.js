4013a331ff8a1fafdd2c6b0266a9960a215d0177
Lakmali Koralage <koralagelak573@gmail.com>
2024-07-26 14:15:11 +0530

    done

--- test02/script.js
+++ test02/script.js
@@ -1,99 +1,82 @@
 let idCounter = 0;
 const elements = [];
 
-$(document).ready(function() {
 
-    // Event listener for add button.
-    $("#addButton").click(function() {
+$(document).ready(function () {
 
-        const newElement = addElement();
-        createNewDivElement(newElement);
+  // Event listener for add button.
+  $("#addButton").click(function () {
 
-    });
-
+    const newElement = addElement();
+    createNewDivElement(newElement);
+    
+  });
 });
 
 
-// Add new element object to elements array
+//  add new element object to elements array
 function addElement() {
 
-    idCounter++;
-    const newDivId = `Div-Element ID : ${idCounter}`;
+  idCounter++;
+  const newDivId = `Div-Element ID : ${idCounter}`;
 
-    const newElement = {
-        id: newDivId,
-        active: false,
-        borderThickness: ""
+  const newElement = {
 
-    };
+    id: newDivId,
+    active: false,
+    borderThickness: "",
+  };
 
-    elements.push(newElement);
-    return newElement;
-
+  elements.push(newElement);
+  return newElement;
 }
 
 
-// Create new div element in the DOM
+//  create new div element in the DOM
 function createNewDivElement(element) {
 
-    const newDiv = $("<div></div>")
-        .addClass("new-div")
-        .attr("data-id", element.id)
-        //  .css("border", `${element.borderThickness}px solid`)
-        .text(`Div ID: ${element.id}`)
-        .appendTo("#divContainer");
+  const newDiv = $("<div></div>")
+    .addClass("new-div")
+    .attr("data-id", element.id)
+    .css("border", `${element.borderThickness}px`)
+    .text(`Div ID: ${element.id}`)
+    .appendTo("#divContainer");
 
-    newDiv.click(function(event) {
+  newDiv.click(function (event) {
 
-        updateDivStatus(element.id, event.ctrlKey);
+    updateDivStatus(element.id, event.ctrlKey);
 
-    });
+  });
 }
+
 
 // Update the status of a div element
 function updateDivStatus(id, isCtrlPressed) {
 
-    elements.forEach((element) => {
+  elements.forEach((element) => {
 
-        const divElement = $(`div[data-id="${element.id}"]`);
+    let divElement = $(`div[data-id="${element.id}"]`);
 
-        if (isCtrlPressed) {
+    if (isCtrlPressed) {
+      // If Ctrl key is pressed
+      if (element.id === id) {
 
-            // If Ctrl key is pressed
-            if (element.id === id) {
+        element.active = !element.active;
+        divElement.toggleClass("border-10");
 
-                if (element.borderThickness > 1) {
-                    // element.borderThickness = 1;
-                    // element.active = false;
-                    // divElement.addClass("border-1 border-4 border-5 border-10")
+      }
+    } else {
 
-                    divElement.toggleClass(" border-5 "); 
-                        
-                } else {
-                    // element.borderThickness = 10;
-                    // element.active = true;
+      // If Ctrl key is not pressed
+      if (element.id === id) {
+        element.active = true;
+        divElement.addClass("border-1");
 
-                     divElement.toggleClass(" border-5");
-                   
-                        
-                }
-            }
-        } else {
-            // If Ctrl key is not pressed
-            if (element.id === id) {
-                // element.borderThickness = 10;
-                // element.active = true;
-
-                divElement.addClass("border-4");
-                    
-                    
-            } else {
-                // element.borderThickness = 1;
-                // element.active = false;
-
-                divElement.removeClass("border-4 ")
-                
-            }
-        }
-    });
+      } else {
+        
+        element.active = false;
+        divElement.removeClass("border-1");
+      }
+    }
+  });
 }
