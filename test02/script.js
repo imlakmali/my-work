25e9695efaaeaf2a96ec09f2bd2f5e1fc0b6751e
Lakmali Koralage <koralagelak573@gmail.com>
2024-07-29 10:06:34 +0530

    add div colors

--- test02/script.js
+++ test02/script.js
@@ -1,92 +1,102 @@
 let idCounter = 0;
 const elements = [];
-let selectedColor= "";
-
+let selectedColor = "";
 
 $(document).ready(function () {
 
-    // Event listener for add button.
+    // Event listener for add button
     $("#addButton").click(function () {
 
         const newElement = addElement();
         createNewDivElement(newElement);
-        
+
     });
 
-
-    // Event listener for color selection 
+    // Event listener for color selection
     $("#favcolor").on("input", function () {
-        selectedColor = $(this).val();
-        $("#colorTarget").css("background-color", selectedColor);
-      });
 
+        selectedColor = $(this).val();
+        $(this).css("background-color", selectedColor);
+
+    });
 });
 
-
-//  add new element object to elements array
+// Add new element object to elements array
 function addElement() {
 
-  idCounter++;
-  const newDivId = `Div-Element ID : ${idCounter}`;
+    idCounter++;
+    const newDivId = `Div-Element ID: ${idCounter}`;
 
-  const newElement = {
+    const newElement = {
+        id: newDivId,
+        active: false,
+        borderThickness: "",
+    };
 
-    id: newDivId,
-    active: false,
-    borderThickness: "",
-  };
-
-  elements.push(newElement);
-  return newElement;
+    elements.push(newElement);
+    return newElement;
 }
 
-
-//  create new div element in the DOM
+// Create new div element in the DOM
 function createNewDivElement(element) {
 
-  const newDiv = $("<div></div>")
-    .addClass("new-div")
-    .attr("data-id", element.id)
-    .css("border", `${element.borderThickness}px`)
-    .css("background-color", selectedColor)
-    .text(`Div ID: ${element.id}`)
-    .appendTo("#divContainer");
+    const newDiv = $("<div></div>")
+        .addClass("new-div")
+        .attr("data-id", element.id)
+        .css("border", `${element.borderThickness}px`)
+        .css("background-color", selectedColor)
+        .text(`Div ID: ${element.id}`)
+        .appendTo("#divContainer");
 
-  newDiv.click(function (event) {
+    newDiv.click(function (event) {
 
-    updateDivStatus(element.id, event.ctrlKey);
+        updateDivStatus(element.id, event.ctrlKey);
 
-  });
+    });
 }
-
 
 // Update the status of a div element
 function updateDivStatus(id, isCtrlPressed) {
 
-  elements.forEach((element) => {
+    elements.forEach((element) => {
 
-    let divElement = $(`div[data-id="${element.id}"]`);
+        let divElement = $(`div[data-id="${element.id}"]`);
 
-    if (isCtrlPressed) {
-      // If Ctrl key is pressed
-      if (element.id === id) {
+        if (isCtrlPressed) {
 
-        element.active = !element.active;
-        divElement.toggleClass("border-10");
+            // If Ctrl key is pressed
+            if (element.id === id) {
 
-      }
-    } else {
+                element.active = !element.active;
+                divElement.toggleClass("border-1");
 
-      // If Ctrl key is not pressed
-      if (element.id === id) {
-        element.active = true;
-        divElement.addClass("border-1");
+                // Apply selected color if active
+                if (element.active) {
 
-      } else {
+                    divElement.css("background-color", selectedColor);
 
-        element.active = false;
-        divElement.removeClass("border-1");
-      }
-    }
-  });
+                } else {
+                    divElement.css("background-color", "#f5f1f1"); 
+                }
+            }
+        } else {
+            // If Ctrl key is not pressed
+            if (element.id === id) {
+
+                element.active = true;
+                divElement.addClass("border-1");
+                divElement.css("background-color", selectedColor);
+
+            } else {
+
+                element.active = false;
+                divElement.removeClass("border-1");
+                divElement.css("background-color", "#f5f1f1"); 
+
+            }
+
+        }
+
+    });
+    
 }
